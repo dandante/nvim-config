8a34f3477c144077200be134d0374a38503e04d7
Dan Tenenbaum <dtenenba@fhcrc.org>
2025-12-26 16:20:15 -0800

    add hotkey to send register in normal mode; keep status bar visible

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -9,7 +9,8 @@ return {
     end
     iron.setup({
       config = {
-        repl_open_cmd = require("iron.view").bottom(20),
+        -- repl_open_cmd = require("iron.view").bottom(20),
+        repl_open_cmd = "horizontal botright 20split",
         repl_definition = {
           -- 1. Explicitly handle empty filetypes (unsaved buffers)
           [""] = { command = { "fish" } },
@@ -32,6 +33,20 @@ return {
     map("n", "<leader>rl", function()
       require("iron.core").send_line()
     end, { desc = "Iron: Send Line" })
+
+    -- Normal Mode: Send a Register
+    -- If you just hit Enter at the prompt, it sends the default (") register
+    map("n", "<leader>rr", function()
+      local reg = vim.fn.input("Register (default: \"): ")
+      if reg == "" then reg = '"' end
+      local content = vim.fn.getreg(reg)
+  -- Ensure we have something to send
+      if content ~= "" then
+        iron.send(nil, content)
+      else
+        print("Register " .. reg .. " is empty")
+      end
+    end, { desc = "Send Register Content" })
 
     -- Send visual selection (if you ever need to send multiple lines)
     map("v", "<leader>rr", function()
