a0015726d1a7d4c22db87e2b846e056ded7e21c7
Dan Tenenbaum <dtenenba@fhcrc.org>
2026-01-26 10:32:12 -0800

    disable swap file

--- init.lua
+++ init.lua
@@ -1,14 +1,11 @@
-
 -- If we wanted to change the leader key from the default (\) we'd uncomment:
 -- SET THIS FIRST
 --vim.g.mapleader = " "
 --vim.g.maplocalleader = " "
 
+vim.opt.swapfile = false
+
 require("plugins")
 
 -- Helper mapping to update plugins (new in 0.12)
 vim.keymap.set("n", "<leader>pu", vim.pack.update, { desc = "Update Plugins" })
-
-
-
-
