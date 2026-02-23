f7d75192b81ec9f55dacdbd29fe2ddc88ac81b72
dtenenba <dtenenba@fhcrc.org>
2026-02-22 23:00:57 -0800

    second attempt to customize noice

--- lua/plugins/noice_config.lua
+++ lua/plugins/noice_config.lua
@@ -1,19 +1,40 @@
 return {
   {
     "folke/noice.nvim",
-    opts = {
-      -- Disables noice rendering for messages and print statements
-      messages = {
-        enabled = false,
-      },
-      -- Disables the fancy notification popups (reverts to standard vim.notify)
-      notify = {
-        enabled = false,
-      },
-      -- Ensures the lsp/print messages go to the classic Neovim "echo" area
-      lsp = {
-        progress = { enabled = false }, -- optional: keeps progress in statusline
-      },
-    },
+    -- 'opts' can be ignored; we'll handle it in 'config'
+    config = function()
+      require("noice").setup({
+        -- 1. This is the big one: it turns off the pop-up messages
+        -- and redirects them to the classic Neovim echo area.
+        messages = {
+          enabled = false,
+        },
+        -- 2. This turns off the fancy "fading" notification popups.
+        notify = {
+          enabled = false,
+        },
+        -- 3. We keep the command line (:) and other features enabled
+        -- unless you want those gone too.
+        cmdline = {
+          enabled = true,
+          view = "cmdline", -- Options: "cmdline_popup" (default) or "cmdline" (bottom)
+        },
+        lsp = {
+          -- We disable the Noice progress bar so it doesn't flicker in the corner
+          progress = { enabled = false },
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.set_良_markdown_lines"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        -- 4. Presets can help simplify things
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = false, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+        },
+      })
+    end,
   },
 }
